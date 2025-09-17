564543f96fea650614841791264c543bce198029
Julia Scarpi Campos <julia.campos@belov.com.br>
2025-09-17 14:47:57 -0300

    Criação das classes Jogo e Program com menu inicial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var jogos = new List<Jogo>();
+    }
+    while true //menu
+    {
+        Console.Clear();
+        Console.WriteLine("===LUDOTECA .NET===");
+        Console.WriteLine("1- Cadastrar jogo");
+        Console.WriteLine("2- Cadastrar membro");
+        Consele.WriteLine("3- Listar jogos");
+        Console.WriteLine("0- Sair");
+        Console.Write("Opção: ");
+
+        string opcao = Console.redline();// le a escolha do usuario
+    }
+}
